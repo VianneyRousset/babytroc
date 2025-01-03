278b5c91f00aa15835751f341c201db77eb6187d
tharvik <tharvik@users.noreply.github.com>
2025-01-03 14:47:16 +0100

    test.sh: spin test compose

--- test.sh
+++ test.sh
@@ -1,10 +1,38 @@
-#!/usr/bin/env sh
+#!/bin/bash -eu
+set -o pipefail
 
-DOCKER_COMPOSE_FILE="compose-test.yaml"
-ENVIRONMENT_FILE=".env"
+readonly COMPOSE_FILE=compose-test.yaml
 
-docker compose \
-  --file "${DOCKER_COMPOSE_FILE}" \
-  --env-file "${ENVIRONMENT_FILE}" \
-  up \
-  --abort-on-container-exit
+export POSTGRES_USER=test
+export POSTGRES_PASSWORD=test
+
+docker_compose() {
+	docker compose -f "$COMPOSE_FILE" "$@"
+}
+
+with_compose() {
+	docker_compose up --detach --build --renew-anon-volumes
+	cleanup() {
+		docker_compose down
+	}
+	trap cleanup EXIT
+
+	export POSTGRES_HOST=localhost
+	export POSTGRES_PORT=$(docker_compose port db 5432 | cut -d: -f2)
+	export IMGPUSH_HOST=localhost
+	export IMGPUSH_PORT=$(docker_compose port images 5000 | cut -d: -f2)
+
+	COUNTER=60 # 1 min timeout
+	while ! docker_compose logs db 2>/dev/null |
+		grep --quiet 'database system is ready to accept connections' ||
+		! curl --silent localhost:${IMGPUSH_PORT}
+	do
+		COUNTER=$((COUNTER - 1))
+		[ $COUNTER -eq 0 ] && break
+		sleep 1
+	done
+
+	"$@"
+}
+
+with_compose pytest "$@"
