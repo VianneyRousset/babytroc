59fae37aee80e7e33800f3eb7b67c690dead7f27
Vianney Rousset <vianney@rousset.com>
2025-01-24 16:52:40 +0100

    add Overlay component

---
+++ gui/components/Overlay.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+const model = defineModel<boolean>();
+
+function onKeyPressed(event: KeyboardEvent) {
+  if (event.key == 'Escape')
+    model.value = false;
+}
+
+onMounted(() => window.addEventListener('keydown', onKeyPressed));
+onUnmounted(() => window.removeEventListener('keydown', onKeyPressed));
+
+</script>
+
+<template>
+  <div class="overlay" :class="{ open: model }" @click.self="model = false">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.overlay {
+
+  display: none;
+
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 20;
+
+  width: 100%;
+  height: 100%;
+
+  background: rgba(0, 0, 0, 0.5);
+
+  &.open {
+    display: block;
+  }
+
+}
+</style>
