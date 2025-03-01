8141eac0a96cb1becbf8c1ec9ba966f0e1d13be2
Vianney Rousset <vianney@rousset.com>
2025-03-01 19:58:57 +0100

    add enums keys in openAPI

--- api/app/enums.py
+++ api/app/enums.py
@@ -1,7 +1,25 @@
 import enum
 
+from pydantic import GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import core_schema
 
-class ChatMessageType(enum.Enum):
+
+class EnumWithMetadata(enum.Enum):
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        json_schema = handler(core_schema)
+        json_schema = handler.resolve_ref_schema(json_schema)
+
+        # add names
+        json_schema["x-enum-varnames"] = [e.name for e in core_schema["members"]]
+
+        return json_schema
+
+
+class ChatMessageType(int, EnumWithMetadata):
     text = 1
     loan_request_created = 2
     loan_request_canceled = 3
@@ -9,17 +27,17 @@ class ChatMessageType(enum.Enum):
     loan_request_rejected = 5
     loan_started = 6
     loan_ended = 7
-    item_not_available = 9
+    item_not_available = 8
     item_available = 9
 
 
-class ReportType(enum.Enum):
+class ReportType(int, EnumWithMetadata):
     user = 1
     item = 2
     chat = 3
 
 
-class LoanRequestState(enum.Enum):
+class LoanRequestState(int, EnumWithMetadata):
     pending = 1
     canceled = 2
     accepted = 3
@@ -27,7 +45,7 @@ class LoanRequestState(enum.Enum):
     executed = 5
 
 
-class ItemQueryAvailability(enum.Enum):
+class ItemQueryAvailability(str, EnumWithMetadata):
     yes = "y"
     no = "n"
     all = "a"
