3842f6ca94451c575c819ba96587759944863f81
Vianney Rousset <vianney@rousset.com>
2025-04-25 12:56:10 +0200

    update test item images

--- api/tests/test_item_images.py
+++ api/tests/test_item_images.py
@@ -2,17 +2,19 @@ from fastapi.testclient import TestClient
 
 
 def test_created_image_can_be_read(
-    client0: TestClient,
-    users: list[int],
-    image_data: str,
+    alice_client: TestClient,
+    alice_items_image_data: str,
 ):
     """Upload image from client 0 and retrieve it from public client."""
 
     # upload image
-    resp = client0.post("/v1/images", files={"file": image_data})
+    resp = alice_client.post(
+        "/v1/images",
+        files={"file": alice_items_image_data},
+    )
     resp.raise_for_status()
     name = resp.json()["name"]
 
     # request item
-    resp = client0.get(f"/v1/images/{name}")
+    resp = alice_client.get(f"/v1/images/{name}")
     resp.raise_for_status()
