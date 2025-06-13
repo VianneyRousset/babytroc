f467d8a6fa203d4e11dbe976e09722bc138f87db
Vianney Rousset <vianney@rousset.com>
2025-06-13 23:07:44 +0200

    add useThrottle

---
+++ gui/app/composables/throttle.ts
@@ -0,0 +1,25 @@
+export function useThrottle<T>(value: Ref<T>, time: MaybeRefOrGetter<number>): { value: Ref<T>, synced: Ref<boolean> } {
+  const synced = ref(true)
+  const result = ref<T>(unref(value)) as Ref<T>
+
+  let timeout = null as null | ReturnType<typeof setTimeout>
+
+  watch(value, (v) => {
+    synced.value = false
+
+    if (timeout) clearTimeout(timeout)
+
+    timeout = setTimeout(
+      () => {
+        result.value = v
+        synced.value = true
+      },
+      toValue(time),
+    )
+  })
+
+  return {
+    synced,
+    value: result,
+  }
+}
