7d6f29c6819a602084566b121d6456cdf806c9f5
Vianney Rousset <vianney@rousset.com>
2025-06-14 16:31:15 +0200

    add field validations in account availability query

--- api/app/schemas/auth/api.py
+++ api/app/schemas/auth/api.py
@@ -1,4 +1,4 @@
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
 
 from app.schemas.base import ApiQueryBase
 
@@ -6,3 +6,25 @@ from app.schemas.base import ApiQueryBase
 class AuthAccountAvailabilityApiQuery(ApiQueryBase):
     name: str | None = None
     email: EmailStr | None = None
+
+    @field_validator("name", mode="before")
+    def validate_name(
+        cls,  # noqa: N805
+        v: str,
+    ) -> str:
+        """Remove leading and trailing whitespace."""
+
+        if isinstance(v, str):
+            return v.strip()
+        return v
+
+    @field_validator("email", mode="before")
+    def validate_email(
+        cls,  # noqa: N805
+        v: str,
+    ) -> str:
+        """Remove leading and trailing whitespace and switch to lowercase."""
+
+        if isinstance(v, str):
+            return v.strip().lower()
+        return v
