d974c8ca8dcf47471e276f5dd2b9cb9d86e4de25
Vianney Rousset <vianney@rousset.com>
2025-06-15 17:53:55 +0200

    add email strip

--- api/app/schemas/auth/create.py
+++ api/app/schemas/auth/create.py
@@ -1,4 +1,4 @@
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
 
 from app.schemas.base import CreateBase
 
@@ -7,3 +7,14 @@ from .base import AuthBase
 
 class AuthAccountPasswordResetAuthorizationCreate(AuthBase, CreateBase):
     email: EmailStr
+
+    @field_validator("email", mode="before")
+    def validate_email(
+        cls,  # noqa: N805
+        v: str,
+    ) -> str:
+        """Remove leading and trailing whitespace and switch to lowercase."""
+
+        if isinstance(v, str):
+            return v.strip().lower()
+        return v
