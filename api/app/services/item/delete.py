eec6cd94f0a8a38c8b53e950cb965bdea803d3ad
Vianney Rousset <vianney@rousset.com>
2025-07-22 17:51:35 +0200

    fix raise ItemNotFoundError

--- api/app/services/item/delete.py
+++ api/app/services/item/delete.py
@@ -1,6 +1,7 @@
 from sqlalchemy import delete
 from sqlalchemy.orm import Session
 
+from app.errors.item import ItemNotFoundError
 from app.models.item import Item
 from app.schemas.item.query import ItemQueryFilter
 
@@ -20,4 +21,7 @@ def delete_item(
 
     stmt = query_filter.apply(stmt)
 
-    db.execute(stmt)
+    res = db.execute(stmt)
+
+    if res.rowcount == 0:
+        raise ItemNotFoundError({"id": item_id})
