167fccb093549fe4c2e031e203e45d1e26beb69a
Vianney Rousset <vianney@rousset.com>
2025-07-22 17:47:49 +0200

    move user creation from clients/ to services/

--- api/app/clients/database/user/__init__.py
+++ api/app/clients/database/user/__init__.py
@@ -1,4 +1,3 @@
-from .create import create_user, insert_user
 from .delete import delete_user
 from .read import (
     get_user,
@@ -14,13 +13,11 @@ from .update import (
 )
 
 __all__ = [
-    "create_user",
     "delete_user",
     "get_user",
     "get_user_by_email",
     "get_user_by_validation_code",
     "get_user_exists",
-    "insert_user",
     "list_users",
     "mark_user_as_validated",
     "update_user",
--- api/app/services/user/__init__.py
+++ api/app/services/user/__init__.py
@@ -1,13 +1,13 @@
 from . import star
-from .create import create_user, create_user_without_validation
+from .create import create_many_users_without_validation, create_user
 from .delete import delete_user
 from .read import get_user, get_user_exists, get_user_private, list_users
 from .report import report_user
 from .update import update_user
 
 __all__ = [
+    "create_many_users_without_validation",
     "create_user",
-    "create_user_without_validation",
     "delete_user",
     "get_user",
     "get_user_exists",
